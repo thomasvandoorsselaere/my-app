fa6c292154ddb851a499bbf014b9ab9c7af1966a
Programmeren\Thomas <vandoorsselaere.thomas@gmail.com>
2017-12-01 10:02:27 +0100

    register and login

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,13 @@
+
+
+export const FIREBASE_CONFIG =  
+   {
+    apiKey: "AIzaSyBgGrbLciRM28NLMYTKwy6tk62trloYdYI",
+    authDomain: "myapp-2863a.firebaseapp.com",
+    databaseURL: "https://myapp-2863a.firebaseio.com",
+    projectId: "myapp-2863a",
+    storageBucket: "myapp-2863a.appspot.com",
+    messagingSenderId: "761832426278"
+  }
+
+  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { AngularFireModule } from 'angularfire2'
+import { AngularFireAuthModule } from "angularfire2/auth"
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -8,6 +10,7 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { FIREBASE_CONFIG } from './app.firebase.config';
 
 @NgModule({
   declarations: [
@@ -18,6 +21,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
