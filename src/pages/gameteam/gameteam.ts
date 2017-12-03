efd0e02dc1470b72f51275d719a2ed94c2503fa0
Programmeren\Thomas <vandoorsselaere.thomas@gmail.com>
2017-12-03 17:37:23 +0100

    teams tonen voor wedstrijd te starten

--- src/pages/gameteam/gameteam.ts
+++ src/pages/gameteam/gameteam.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ProvidersTeamsProvider } from '../../providers/providers-teams/providers-teams';
+import { Team } from '../../models/team';
 
 /**
  * Generated class for the GameteamPage page.
@@ -14,12 +16,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'gameteam.html',
 })
 export class GameteamPage {
+  teams: Team[]
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    private teamProvider: ProvidersTeamsProvider,
+    public navCtrl: NavController,
+     public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad GameteamPage');
+    ionViewDidLoad() {
+      console.log('ionViewDidLoad GameteamPage');
+       this.teamProvider.getTeams().subscribe(teams => {
+      // console.log(teams)
+       this.teams = teams
+    })
   }
-
 }
+
